119cc2ced7d5fa7eeef994cb577a9434ba9beca6
Junill Jang <jang020407@naver.com>
2025-08-05 17:48:04 +0900

    dotenv 삭제

--- update-today-solved.js
+++ update-today-solved.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 import fetch from 'node-fetch'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
